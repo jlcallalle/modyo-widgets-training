a998b8b7edfbf0a05878eaf2ecb07f3128d5786c
Jorge Callalle <jlcallalle@gmail.com>
2021-06-30 09:38:14 -0500

    template modyo widgets vue

---
+++ src/components/BoxComponent.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="col-md-4 text-center componente">
+    <h3 class="hola h4 mt-3">
+      Box Componented
+    </h3>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'BoxComponent',
+  data: () => ({
+  }),
+};
+</script>
+
+<style scoped lang="scss">
+.hola {
+  background-color:rgba(255,0,0,0.2);
+}
+</style>
